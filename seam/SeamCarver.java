08f8d933c0976569b26524d7ada0f36d55fe6feb
Ehehing <ruiruithing@gmail.com>
2025-03-25 14:38:03 -0700

    um

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -15,7 +15,43 @@ public class SeamCarver {
 
         //calculate initial energy
 
+        for (int row = 0; row<picture.height(); row++) {
+
+            for (int col = 0; col < picture.width(); col++) {
+
+                energyField[row][col] = calculateEnergy(row, col);
+
+            }
+
+        }
+
         calculateEnergyField();
+
+        energyField = new double[picture.height()][picture.width()];
+        transposePicture();
+
+
+    }
+
+    private double calculateEnergy(int row, int col) {
+        //fetch RGB values for top bottom, left right? how to fetch RGB
+
+        if (row == 0 || row == picture.height()-1 || col == 0 || col == picture.width()) {
+            return 1000;
+        }
+
+        int x1 = picture.get(col-1, row).getRed() - picture.get(col+1, row).getRed();
+        int y1 = picture.get(col-1, row).getGreen() - picture.get(col+1, row).getGreen();
+        int z1 = picture.get(col-1, row).getBlue() - picture.get(col+1, row).getBlue();
+        int x2 = picture.get(col, row-1).getRed() - picture.get(col, row+1).getRed();
+        int y2 = picture.get(col, row-1).getGreen() - picture.get(col, row+1).getGreen();
+        int z2 = picture.get(col, row-1).getBlue() - picture.get(col, row+1).getBlue();
+
+        return Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2) + Math.pow(z1, 2)
+        + Math.pow(x2, 2) + Math.pow(y2, 2) + Math.pow(z2, 2));
+
+
+
 
     }
 
@@ -29,6 +65,18 @@ public class SeamCarver {
             //do math
 
     }
+
+    private void transposePicture() {
+        Picture tempPicture = new Picture(picture.height(), picture.width());
+        for (int row = 0; row < picture.width(); row++) {
+            for (int col = 0; col < picture.height(); col++) {
+
+                tempPicture.setARGB(col, row, picture.getARGB(row, col));
+
+            }
+        }
+        picture = tempPicture;
+    }
  
     // current picture
     public Picture picture() {
@@ -37,12 +85,12 @@ public class SeamCarver {
  
     // width of current picture
     public int width() {
-        return 0;
+        return picture.width();
     }
  
     // height of current picture
     public int height() {
-        return 0;
+        return picture().height();
     }
  
     // energy of pixel at column x and row y
@@ -57,6 +105,21 @@ public class SeamCarver {
  
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
+        //Traverse matric to build the distTo and edgeTo
+        int[][] edgeTo = new int[picture.height()][picture.width()];
+        double[][] distTo = new double[picture.height()][picture.width()];
+
+        //for all rows
+        //for all cols
+        //set initial diskTo as POSITIVE INFINITY
+        //if row == 0 - sset diskTo to 1000 since it is on the border
+
+        //Consider all verticies in topological order
+        //Relas all edges pointing from that vertex
+        
+        //for all rows and cols, 
+        //Check below, left, right, above
+
         return null;
     }
  
@@ -67,6 +130,25 @@ public class SeamCarver {
  
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (vertical) {
+            transposePicture();
+            vertical = false;
+        }
+        removeSeam(seam);
+    }
+
+    private void removeSeam (int [] seam) {
+        if (seam == null) {
+            throw new IllegalArgumentException("Null argument to removeSeam.\n");
+        }
+        if (seam.length != picture.height()) {
+            throw new IllegalArgumentException("seam wrong length.\n");
+        }
+
+        Picture tempPicture = new Picture(picture.width() - 1, picture.height());
+        //seam removal in 2D for loop
+        picture = tempPicture;
+        
 
     }
  
